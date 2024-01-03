0f6e898b3eb58134c18e7e403bbd01092622e475
hsiye <155467021+hsiye@users.noreply.github.com>
2024-01-03 00:05:50 +0000

    Add variables

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Wsg!")
+name = input("What is your name?")
+age = input(int("How old are you?"))
+ask = input("How can I help you today?: Option 1, Option 2, Exit")
+
+if ask == "Option 1":
+  print("You have select)
